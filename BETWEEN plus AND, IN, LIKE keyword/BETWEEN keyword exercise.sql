f95f7eb643f51b4737e4d73c6a388da8cc6ad87e
Obsone <jsdcone49@gmail.com>
2024-05-27 12:54:58 +0800

    BETWEEN keyword exercise

---
+++ BETWEEN plus AND, IN, LIKE keyword/BETWEEN keyword exercise.sql
@@ -0,0 +1,14 @@
+-- Who between the ages of 30 and 50 has an income less than 50 000?
+-- (include 30 and 50 in the results)
+
+/*
+* Write your query here
+*/
+SELECT age, income FROM customers
+WHERE (age BETWEEN 30 AND 50) AND income < 50000
+-- What is the average income between the ages of 20 and 50? (Including 20 and 50)
+/*
+* Write your query here
+*/ 
+SELECT avg(income) FROM customers
+WHERE age BETWEEN 20 AND 50
