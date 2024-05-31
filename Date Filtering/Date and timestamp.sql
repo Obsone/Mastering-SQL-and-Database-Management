ddde0e2005246c5c652c63420bcc78fbd7a94493
Obsone <jsdcone49@gmail.com>
2024-05-31 14:50:06 +0800

    continuation exercise date functions
    Review and practice more on this, many use cases

--- Date Filtering/Date and timestamp.sql
+++ Date Filtering/Date and timestamp.sql
@@ -28,7 +28,42 @@ WHERE EXTRACT (YEAR FROM age( birth_date)) > 60;
 * Table: employees
 * Question: How many employees where hired in February?
 */
-
 SELECT first_name, hire_date FROM employees
 WHERE EXTRACT( MONTH FROM hire_date) = 02; 
 
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees were born in november?
+*/
+SELECT birth_date FROM employees
+WHERE EXTRACT (MONTH FROM birth_date) = 11;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Who is the oldest employee? (Use the analytical function MAX)
+*/
+SELECT first_name, birth_date FROM employees
+WHERE birth_date = (SELECT min(birth_date) FROM employees);
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made in January 2004?
+*/
+-- 3 POSSIBLE SYNTAX
+
+SELECT count(orderdate) FROM orders
+WHERE EXTRACT (YEAR FROM orderdate) = 2004
+AND
+EXTRACT (MONTH FROM orderdate) = 01;
+
+SELECT count(orderid)
+FROM orders
+WHERE DATE_TRUNC('month', orderdate) = date '2004-01-01';
+
+SELECT date_trunc('month', orderdate) AS MONTH, COUNT(*) AS order_count
+FROM orders
+GROUP BY date_trunc('month', orderdate)
+ORDER BY MONTH; 
