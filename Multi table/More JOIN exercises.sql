13668f84162f50d45d35f4f38001f308c21abd8f
Obsone <jsdcone49@gmail.com>
2024-06-12 22:35:50 +0800

    iteration practice of inner join keyword

--- Multi table/More JOIN exercises.sql
+++ Multi table/More JOIN exercises.sql
@@ -5,10 +5,14 @@
 * ordered by orderid
 */
 
-SELECT c.firstname, c.lastname, o.orderid FROM orders AS o
+SELECT  c.firstname,
+        c.lastname,
+        c.state,
+        o.orderid FROM orders AS o
 INNER JOIN customers AS c ON o.customerid = c.customerid
 WHERE c.state IN ('NY', 'OH', 'OR')
 ORDER BY o.orderid;
+
 
 
 /*
@@ -17,9 +21,10 @@ ORDER BY o.orderid;
 * Question: Show me the inventory for each product
 */
 
-SELECT p.prod_id, i.quan_in_stock
+SELECT  p.prod_id,
+        i.quan_in_stock
 FROM products AS p
-INNER JOIN inventory AS i ON p.prod_id = i.prod_id 
+INNER JOIN inventory AS i ON p.prod_id = i.prod_id;
 
 
 /*
@@ -28,7 +33,8 @@ INNER JOIN inventory AS i ON p.prod_id = i.prod_id
 * Question: Show me for each employee which department they work in
 */
 
-SELECT e.first_name, dp.dept_name
+SELECT  e.first_name,
+        dp.dept_name
 FROM employees AS e
 INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
-INNER JOIN departments AS dp ON dp.dept_no = de.dept_no
+INNER JOIN departments AS dp ON dp.dept_no = de.dept_no;
