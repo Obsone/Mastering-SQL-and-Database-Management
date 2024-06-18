2ef70b8f4fd6cfb14de9bceda19b9762b2de7370
Obsone <jsdcone49@gmail.com>
2024-06-18 23:41:46 +0800

    grouping sets -- answered

--- Grouping Sets/Grouping exercises.sql
+++ Grouping Sets/Grouping exercises.sql
@@ -1,11 +1,30 @@
 /*
-*  Calculate the total amount of employees per department and the total using grouping sets
+*  Calculate the total amount of employees per department using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
+
+SELECT GROUPING(e.dept_no), e.dept_no, COUNT(e.emp_no)
+FROM public.dept_emp AS e
+GROUP BY
+	GROUPING SETS (
+		(e.dept_no),
+     		()
+	)
+ORDER BY e.dept_no
+
 
 /*
 *  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
+SELECT GROUPING(de.dept_no), de.dept_no, AVG(e.salary)
+FROM public.salaries AS e
+JOIN public.dept_emp AS de USING (emp_no)
+GROUP BY
+	GROUPING SETS (
+		(de.dept_no),
+     	()
+	)
+ORDER BY de.dept_no
