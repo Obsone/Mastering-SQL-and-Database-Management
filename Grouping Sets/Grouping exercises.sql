f2eb4d44ced5cf1d9a539527d0fc112c8e7790bb
Obsone <jsdcone49@gmail.com>
2024-06-19 02:39:30 +0800

    grouping sets -- done practice

--- Grouping Sets/Grouping exercises.sql
+++ Grouping Sets/Grouping exercises.sql
@@ -3,7 +3,14 @@
 *  Database: Employees
 *  Table: Employees
 */
+SELECT  count(e.emp_no),
+        d.dept_no
+FROM employees AS e
+INNER JOIN dept_emp AS d USING(emp_no)
+GROUP BY d.dept_no;
 
+--differenciate your work ^up^--
+ 
 SELECT GROUPING(e.dept_no), e.dept_no, COUNT(e.emp_no)
 FROM public.dept_emp AS e
 GROUP BY
@@ -11,7 +18,7 @@ GROUP BY
 		(e.dept_no),
      		()
 	)
-ORDER BY e.dept_no
+ORDER BY e.dept_no;
 
 
 /*
@@ -20,11 +27,11 @@ ORDER BY e.dept_no
 *  Table: Employees
 */
 SELECT GROUPING(de.dept_no), de.dept_no, AVG(e.salary)
-FROM public.salaries AS e
-JOIN public.dept_emp AS de USING (emp_no)
+FROM salaries AS e
+INNER JOIN dept_emp AS de USING (emp_no)
 GROUP BY
-	GROUPING SETS (
-		(de.dept_no),
-     	()
-	)
-ORDER BY de.dept_no
+    GROUPING SETS (
+        (de.dept_no),
+        ()
+    )
+ORDER BY de.dept_no;
