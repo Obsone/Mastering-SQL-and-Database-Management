86b3ca1f58627ae2b7991b56a01067c3a8aaa646
Obsone <jsdcone49@gmail.com>
2024-06-20 23:22:58 +0800

    rollup function example added
    keynote, it does not include non hierachical groupings unlike grouping sets which you can add it

--- Rollup/Rollup exercise.sql
+++ Rollup/Rollup exercise.sql
@@ -27,3 +27,21 @@ ORDER BY
     EXTRACT (YEAR FROM orderdate),
     EXTRACT (MONTH FROM orderdate),
     EXTRACT (DAY FROM orderdate);
+    
+--     This example is too long and a lot of work that is why we will use ROLLUP (see the difference)
+
+SELECT  EXTRACT (YEAR FROM orderdate) AS "year" ,
+        EXTRACT (MONTH FROM orderdate) AS "month",
+        EXTRACT (DAY FROM orderdate) AS "day" ,
+        sum(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+    ROLLUP (
+        EXTRACT (YEAR FROM orderdate),
+        EXTRACT (MONTH FROM orderdate),
+        EXTRACT (DAY FROM orderdate)
+    )
+ORDER BY
+    EXTRACT (YEAR FROM orderdate),
+    EXTRACT (MONTH FROM orderdate),
+    EXTRACT (DAY FROM orderdate);
