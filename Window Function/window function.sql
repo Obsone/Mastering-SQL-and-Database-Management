9b4c7d49aab7f9a4066ece3d91bc974d267300d2
Obsone <jsdcone49@gmail.com>
2024-06-20 23:26:53 +0800

    learning Window function -- notes

---
+++ Window Function/window function.sql
@@ -0,0 +1,9 @@
+-- how do we apply functions against a set of rows related to the current row?
+
+-- average salary per department and then add the avg to every salary so we could
+-- visually see how much each employee is from the average 
+
+-- WINDOW FUNCTION - create a new column based on functions performed on a subset or 'window' of the data
+-- https://www.postgresql.org/docs/12/tutorial-window.html
+
+ 
