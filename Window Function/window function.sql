be19d3fc3abd0c7b0fdc12cf3923a6c0938510a1
Obsone <jsdcone49@gmail.com>
2024-06-21 23:13:08 +0800

    window and partition by -- notes

--- Window Function/window function.sql
+++ Window Function/window function.sql
@@ -6,4 +6,24 @@
 -- WINDOW FUNCTION - create a new column based on functions performed on a subset or 'window' of the data
 -- https://www.postgresql.org/docs/12/tutorial-window.html
 
- 
+-- Let's get max salary and show also each individual employee salary
+SELECT
+    *,
+    max(salary) OVER()
+FROM salaries
+-- limit 100;
+
+
+-- PARTITION BY - divide rows into groups to apply the function against (optional)
+SELECT
+    *,
+    avg(salary) OVER(
+        PARTITION BY d.dept_name
+    )
+FROM salaries
+JOIN dept_emp AS de USING (emp_no)
+JOIN departments AS d USING (dept_no)
+WHERE d.dept_name IN ('Marketing', 'Finance', 'Human Resources', 'Sales', 'Quality Management', 'Research')
+-- not on the lession. You use the where clause to get a glimpse of every record in the department coz
+-- the query or record output is more than 3million, you'll notice you get 1M records maybe because of the limitation of valentina studio
+
