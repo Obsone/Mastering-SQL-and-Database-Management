8f4c2fd9b1a7e12f2acd46082988188ef807990e
Obsone <jsdcone49@gmail.com>
2024-07-02 23:52:55 +0800

    Using case statement exercise

--- Conditional statements/conditional statement exercise.sql
+++ Conditional statements/conditional statement exercise.sql
@@ -1,3 +1,22 @@
+--EXERCISE--
+/*
+* Database: Store
+* Table: products
+* Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
+* if it's between 10 and 20 you show 'average' 
+* and of is lower than or equal to 10 you show 'cheap'.
+*/
+ SELECT
+    prod_id,
+    price,
+    title,
+    CASE
+        WHEN price > 20 THEN 'expensive'
+        WHEN price BETWEEN 10 AND 20 THEN 'average'
+        WHEN price <= 10 THEN 'cheap'
+    END AS "price class"
+FROM products;
+
 --PRACTICE--
 /*What if you only want to select something when a certain criteria is met?
 */
