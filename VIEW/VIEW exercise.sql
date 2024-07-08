87040e5c72cdc01a694fa0391e4cf6b32fb52658
Obsone <jsdcone49@gmail.com>
2024-07-08 14:17:38 +0800

    VIEW exercise

--- VIEW/VIEW exercise.sql
+++ VIEW/VIEW exercise.sql
@@ -5,7 +5,14 @@
 */
 
 CREATE VIEW "90-95" AS
--- ...
+SELECT *
+FROM employees AS e
+WHERE EXTRACT (YEAR FROM e.hire_date) BETWEEN 1990 AND 1995
+ORDER BY e.emp_no;
+
+--my answer--
+SELECT  emp_no, hire_date FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1995-12-31';
 
 /*
 *  Create a view "bigbucks" that:
@@ -14,4 +21,8 @@ CREATE VIEW "90-95" AS
 */
 
 CREATE VIEW "bigbucks" AS
--- ...
+SELECT e.emp_no, s.salary
+FROM employees AS e
+JOIN salaries AS s USING(emp_no)
+WHERE s.salary > 80000
+ORDER BY s.salary;
