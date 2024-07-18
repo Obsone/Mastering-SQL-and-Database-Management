cf37d72b04bb4b33900fba5ba4886ce2532f7c84
Obsone <jsdcone49@gmail.com>
2024-07-19 02:47:10 +0800

    INDEX types -- lesson

---
+++ Index/index.sql
@@ -0,0 +1,34 @@
+-- creating an index
+CREATE UNIQUE INDEX index_name
+ON table_name (column1, column2, ...);
+
+--delete index
+DROP INDEX index_name
+
+-- when to use
+/*
+-index foreign keys
+-index primary keys and unique columns
+-index on columns that end up in the order by/where clause often
+*/
+
+-- when not to use
+/*
+• DON'T ADD AN INDEX JUST to add INDEX
+• DON'T USE INDEXES ON SMALL TABLES
+• DON'T USE ON TABLES that are UPDATED FREQUENTLY
+• DON'T USE ON COLUMNS THAT CAN CONTAIN NULL values
+• DON'T USE ON COLUMNS THAT HAVE large values
+*/
+
+-- INDEX TYPES
+/*Single-column - most frequently used column in a query | use when retrieving data that satisfies one condition
+Multi-column - '' | use when retrieving data that satisfies multiple condition
+Unique - for speed and integrity
+Partial - index over a subset of a table
+implicit - automatically created by the database: primary and unique key
+*/
+
+--partial example
+CREATE INDEX index_name
+ON table_name (exppression)
